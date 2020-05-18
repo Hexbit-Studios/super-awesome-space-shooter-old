e2d852659d023680e31c7efa871900e82d5eaef5
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-05-17 23:11:32 -0400

    feat: allow slight thrust control to movement

--- Scripts/PlayerShip.gd
+++ Scripts/PlayerShip.gd
@@ -1,26 +1,24 @@
 extends KinematicBody2D
 
 export(int) var SPEED = 100
+export(Vector2) var STARTING_DIRECTION = Vector2(1, 0)
 
-var direction = Vector2.ZERO
-var apply_thrust = false
+var direction = Vector2()
+var thrust = 0
 var velocity = Vector2.ZERO
+
+func _ready():
+	direction = STARTING_DIRECTION
 
 func _process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	thrust = Input.get_action_strength("apply_thrust")
 	
 	if input_vector != Vector2.ZERO:
 		direction = input_vector.normalized()
 		rotation = atan2(direction.y, direction.x);
-	
-	if Input.is_action_pressed("ui_accept"):
-		apply_thrust = true
-		
-	if Input.is_action_just_released("ui_accept"):
-		apply_thrust = false
 
 func _physics_process(delta):
-	if apply_thrust:
-		velocity = move_and_slide(direction * SPEED)
+	velocity = move_and_slide(direction * thrust * SPEED)
