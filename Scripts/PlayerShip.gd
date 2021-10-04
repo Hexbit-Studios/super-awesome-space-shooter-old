457784293c7ec5505c1a406af13006ec2e99d688
Jack Kinsey <jack.w.kinsey@gmail.com>
2021-10-04 10:20:13 -0400

    feat: implement fluid movement & smooth deadzones
    Movement no longer feels clunky and ship doesn't snap to axis when
    stoping or turning.

--- Scripts/PlayerShip.gd
+++ Scripts/PlayerShip.gd
@@ -7,29 +7,36 @@ export(Vector2) var STARTING_DIRECTION = Vector2(1, 0)
 
 onready var primary_fire_point = $FirePointPrimary
 
-var direction = Vector2()
-var thrust = 0
+var direction = Vector2.ZERO
+var unit_circle_direction = Vector2.ZERO
 var velocity = Vector2.ZERO
+var prev_unit_circle_direction
 
 func _ready():
 	direction = STARTING_DIRECTION
 
 func _process(_delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	thrust = Input.get_action_strength("apply_thrust")
+
+	var horizontal = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var vertical = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	direction = Vector2(horizontal, vertical).clamped(1)
 	
+	unit_circle_direction = Vector2.ZERO
 	
-	if Input.is_action_just_pressed("fire_primary"):
-		fire_primary_weapon()
+	if abs(direction.x) > 0.7 || abs(direction.y) > 0.7:
+		unit_circle_direction = direction
+	elif abs(direction.x) <= 0.7 && abs(direction.y) <= 0.7:
+		unit_circle_direction = Vector2.ZERO
+	elif prev_unit_circle_direction != null:
+		unit_circle_direction = prev_unit_circle_direction
 	
-	if input_vector != Vector2.ZERO:
-		direction = input_vector.normalized()
-		rotation = atan2(direction.y, direction.x);
+	prev_unit_circle_direction = unit_circle_direction
+	
+	if unit_circle_direction != Vector2.ZERO:
+		rotation = atan2(unit_circle_direction.y, unit_circle_direction.x)
 
 func _physics_process(_delta):
-	velocity = move_and_slide(direction * thrust * SPEED)
+	velocity = move_and_slide(unit_circle_direction * SPEED)
 
 func fire_primary_weapon():
 	var laser = Laser.instance()
